722150c1d4cc4ba303ac08ad1d598bc11366fe46
Martin Buchholz <32457975+martinbuchholz@users.noreply.github.com>
2025-06-25 19:36:35 +0200

    Update test.ts

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -1,4 +1,4 @@
 import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { TestBed } from '@angular/core/testing';
 
-TestBed.initTestEnvironment(platformBrowserDynamicTesting());
+//TestBed.initTestEnvironment(platformBrowserDynamicTesting());
